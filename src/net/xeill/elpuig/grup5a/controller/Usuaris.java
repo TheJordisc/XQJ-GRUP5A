768d11cd7ec84a566120872a4967e1a2ca190138
TheJordisc <jsolaceada@gmail.com>
2018-03-22 14:52:11 +0100

    Stuff

--- src/net/xeill/elpuig/grup5a/controller/Usuaris.java
+++ src/net/xeill/elpuig/grup5a/controller/Usuaris.java
@@ -3,6 +3,9 @@ package net.xeill.elpuig.grup5a.controller;
 import net.xeill.elpuig.grup5a.model.ArxiuUsuaris;
 
 import javax.xml.xquery.XQConnection;
+import javax.xml.xquery.XQException;
+import javax.xml.xquery.XQExpression;
+import javax.xml.xquery.XQResultSequence;
 
 public class Usuaris {
     XQConnection conn;
@@ -11,8 +14,34 @@ public class Usuaris {
         this.conn=conn;
     }
 
-    public void queryTotsUsuaris() {
+    public void queryTotsUsuaris() throws XQException {
+        XQExpression query1 = conn.createExpression();
+        XQResultSequence query1Result = query1.executeQuery("for $a in doc(\"/db/GRUP5A/arxius-usuaris_.xml\")/xml/arxius-usuaris\n" +
+                "order by xs:integer($a/UsuarisSalesDeConsulta/text()) descending\n" +
+                "return concat($a/Equipament/text(),\",\",$a/Districte/text(),\",\",$a/TipusEquipament/text(),\",\",$a/Any/text(),\",\",$a/Ambit/text(),\n" +
+                "               \",\",$a/Titularitat/text(),\",\",$a/Latitud/text(),\",\",$a/Longitud/text(),\",\",$a/UsuarisSalesDeConsulta/text(),\",\",$a/Nota/text())");
 
+        while (query1Result.next()) {
+            String[] stringArray =  query1Result.getItemAsString(null).split(",");
+            System.out.println("Equipament: " + stringArray[0]);
+            System.out.println("Districte: " + stringArray[1]);
+            System.out.println("Tipus d'equipament: " + stringArray[2]);
+            System.out.println("Any: " + stringArray[3]);
+            System.out.println("Àmbit: " + stringArray[4]);
+            System.out.println("Titularitat: " + stringArray[5]);
+            System.out.println("Latitud: " + stringArray[6]);
+            System.out.println("Longitud: " + stringArray[7]);
+            System.out.println("Usuaris de sales de consulta: " + stringArray[8]);
+
+            String nota;
+            if(stringArray[9].isEmpty()) {
+                nota="(No hi ha cap nota)";
+            } else {
+                nota=stringArray[9];
+            }
+
+            System.out.println("Nota: " + nota);
+        }
     }
 
     public void querySumaUsuaris() {
@@ -31,8 +60,9 @@ public class Usuaris {
         //TODO: Pedir datos y crear instancia en menú
     }
 
-    public void deleteArxiuUsuaris(String queryField) {
+    public boolean deleteArxiuUsuaris(String queryField) {
         //TODO: Buscar por algún campo desde menú
+        return false;
     }
 
     public void updateArxiuUsuaris(String queryField, String newValue) {
